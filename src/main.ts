b1562124048042846c25224b6071b22ec494b0d3
jinfengwhy <xingzhe290@foxmail.com>
2021-12-18 18:16:14 +0800

    style: add vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// 注册vue-router插件
+app.use(router)
+
+// 挂载dom
+app.mount('#app')
